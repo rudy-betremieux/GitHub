a73ad81ccbbbff5d37d1bb8cac6c1e3137909e3d
rudy <rudi.betremieux@gmail.com>
2017-07-03 13:43:08 +0200

    ex9

---
+++ 2eme-cours-javascript/ex9.js
@@ -0,0 +1,13 @@
+var printForm = fonction() {
+var box = document.getElementbyId("box");
+box.innerHTML =
+"prénom:" + document.getElementbyId("firstname").value + "<br>" +
+"nom:" + document.getElementbyId("lastname").value + "<br>" +
+"email:" + document.getElementbyId("email").value + "<br>" +
+"anniversaire:" + document.getElementbyId("birthday").value + "<br>" +
+"mot de passe:" + document.getElementbyId("password").value + "<br>" +
+"avatar:" + document.getElementbyId("avatar").value + "<br>" +
+"anniversaire:" + document.getElementbyId("birthday").value + "<br>" +
+"fichiers:" + document.getElementbyId("files").value + "<br>" +
+
+}
